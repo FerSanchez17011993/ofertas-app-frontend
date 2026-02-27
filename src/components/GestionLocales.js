9301c802f48c514fb84920b7967e1ad802a1f0cf
Fernando Sánchez <fersanchez17011993@gmail.com>
2026-02-27 11:06:03 -0300

    Configuración final para deploy en Vercel

--- src/components/GestionLocales.js
+++ src/components/GestionLocales.js
@@ -7,7 +7,7 @@ import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Corregir iconos de Leaflet
+
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 let DefaultIcon = L.icon({ iconUrl: icon, shadowUrl: iconShadow, iconSize: [25, 41], iconAnchor: [12, 41] });
